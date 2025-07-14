6e5e9bca7b759a840bd74de28710265e74d47220
mryorn84 <mrkiarash84@gmail.com>
2025-07-15 00:08:08 +0330

    Update HideAppWorker.java

--- app/src/main/java/com/example/silentmodedisabler/HideAppWorker.java
+++ app/src/main/java/com/example/silentmodedisabler/HideAppWorker.java
@@ -1,4 +1,30 @@
-ComponentName componentName = new ComponentName(context, MainActivity.class);
-p.setComponentEnabledSetting(componentName,
-    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-    PackageManager.DONT_KILL_APP);
+package com.example.silentmodedisabler;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class HideAppWorker extends Worker {
+
+    public HideAppWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Context context = getApplicationContext();
+        PackageManager p = context.getPackageManager();
+
+        ComponentName componentName = new ComponentName(context, MainActivity.class);
+        p.setComponentEnabledSetting(componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+
+        return Result.success();
+    }
+}
